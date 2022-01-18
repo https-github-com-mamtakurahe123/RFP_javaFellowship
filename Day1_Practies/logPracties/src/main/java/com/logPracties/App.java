680fa1da4f769376219d22c4db5f4a21102947f4
Deelip Kurahe <deelip.markacommunications@gmail.com>
2022-01-18 09:24:44 +0530

    day 1 and day 2 assigment commited

---
+++ Day1_Practies/logPracties/src/main/java/com/logPracties/App.java
@@ -0,0 +1,24 @@
+package com.logPracties;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	private static final Logger LOG = LogManager.getLogger(App.class);
+    public static void main( String[] args )
+    {
+		String message = "Hello World!";
+		LOG.debug(message + "will be printed on Debug");
+		LOG.info(message + "will be printed on Debug");
+		LOG.warn(message + "will be printed on Debug");
+		LOG.error(message + "will be printed on Debug");
+		LOG.fatal(message + "will be printed on Debug");
+		LOG.info( "Appache String {}." , message);
+        System.out.println(message);
+    }
+}
---
+++ Day1_Practies/shortcut/src/shortcut/shortCut.java
@@ -0,0 +1,7 @@
+package shortcut;
+
+public class shortCut {
+	public static void main(String[] args) {
+		System.out.println("hello java world");
+	}
+}
